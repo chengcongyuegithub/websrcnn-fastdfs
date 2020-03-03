49c11e4c13dd68ff25cf44bdfccbe3ef84ab16f3
chengcongyue <chengcongyue@sina.com>
2020-03-03 14:52:15 +0800

    修改SRCNN的模型，增加padding，原图和处理过的图片大小相同

--- srcnn/model.py
+++ srcnn/model.py
@@ -2,6 +2,7 @@ import os
 import tensorflow as tf
 from srcnn.utils import *
 
+
 # 定义SRCNN类
 class SRCNN(object):
     def __init__(self, sess, checkpoint_dir):
@@ -11,12 +12,7 @@ class SRCNN(object):
 
     # 搭建网络
     def build_model(self):
-        self.images = tf.placeholder(tf.float32, [None, 33, 33, 1], name='images')
-        self.labels = tf.placeholder(tf.float32, [None, 21, 21, 1], name='labels')
-        # 第一层CNN：对输入图片的特征提取。（9 x 9 x 64卷积核）
-        # 第二层CNN：对第一层提取的特征的非线性映射（1 x 1 x 32卷积核）
-        # 第三层CNN：对映射后的特征进行重建，生成高分辨率图像（5 x 5 x 1卷积核）
-        # 权重
+        self.test = tf.placeholder(tf.float32, [None, None, None, None], name='test')
         self.weights = {
             # 论文中为提高训练速度的设置 n1=32 n2=16
             'w1': tf.Variable(tf.random_normal([9, 9, 1, 64], stddev=1e-3), name='w1'),
@@ -30,8 +26,6 @@ class SRCNN(object):
             'b3': tf.Variable(tf.zeros([1]), name='b3')
         }
         self.pred = self.model()
-        # 以MSE作为损耗函数
-        self.loss = tf.reduce_mean(tf.square(self.labels - self.pred))
         self.saver = tf.train.Saver()
 
     def superresolution(self, img):
@@ -40,9 +34,8 @@ class SRCNN(object):
             print(" [*] Load SUCCESS")
         else:
             print(" [!] Load failed...")
-        data, color, nx, ny = parpare_for_superresolution(self.sess, img)
-        conv_out = self.pred.eval({self.images: data})
-        conv_out = merge(conv_out, [nx, ny])
+        data, color = preprocess_for_superresolution(img)
+        conv_out = self.pred.eval({self.test: data})
         conv_out = conv_out.squeeze()
         result_bw = revert(conv_out)
         result = np.zeros([result_bw.shape[0], result_bw.shape[1], 3], dtype=np.uint8)
@@ -57,15 +50,13 @@ class SRCNN(object):
             print(" [*] Load SUCCESS")
         else:
             print(" [!] Load failed...")
-        data, label, color, nx, ny = parpare_for_upscaling(self.sess, img, scale)
+        data, color = preprocess_for__upscaling(img, scale)
         if flag:
-            conv_out = self.pred.eval({self.images: data})
-            conv_out = merge(conv_out, [nx, ny])
+            conv_out = self.pred.eval({self.test: data})
             result_bw = conv_out.squeeze()
             result_bw = revert(result_bw)
         else:
-            conv_out = merge(label, [nx, ny])
-            result_bw = conv_out.squeeze()
+            result_bw = data.squeeze()
             result_bw = revert(result_bw)
         result = np.zeros([result_bw.shape[0], result_bw.shape[1], 3], dtype=np.uint8)
         result[:, :, 0] = result_bw
@@ -75,10 +66,10 @@ class SRCNN(object):
 
     def model(self):
         conv1 = tf.nn.relu(
-            tf.nn.conv2d(self.images, self.weights['w1'], strides=[1, 1, 1, 1], padding='VALID') + self.biases['b1'])
+            tf.nn.conv2d(self.test, self.weights['w1'], strides=[1, 1, 1, 1], padding='SAME') + self.biases['b1'])
         conv2 = tf.nn.relu(
-            tf.nn.conv2d(conv1, self.weights['w2'], strides=[1, 1, 1, 1], padding='VALID') + self.biases['b2'])
-        conv3 = tf.nn.conv2d(conv2, self.weights['w3'], strides=[1, 1, 1, 1], padding='VALID') + self.biases['b3']
+            tf.nn.conv2d(conv1, self.weights['w2'], strides=[1, 1, 1, 1], padding='SAME') + self.biases['b2'])
+        conv3 = tf.nn.conv2d(conv2, self.weights['w3'], strides=[1, 1, 1, 1], padding='SAME') + self.biases['b3']
         return conv3
 
     def load(self, checkpoint_dir):
--- srcnn/utils.py
+++ srcnn/utils.py
@@ -3,35 +3,6 @@ import cv2 as cv
 import scipy.misc
 import scipy.ndimage
 
-def modcrop_small(image):
-    padding2 = 6
-    if len(image.shape) == 3:
-        h, w, _ = image.shape
-        temp = h - 33 + 1
-        if temp % 21 == 0:
-            h = temp + padding2
-        else:
-            h = (h - 33 + 1) // 21 * 21 + 21 + padding2
-        temp = w - 33 + 1
-        if temp % 21 == 0:
-            w = temp + padding2
-        else:
-            w = (w - 33 + 1) // 21 * 21 + 21 + padding2
-        image1 = image[padding2:h, padding2:w, :]
-    else:
-        h, w = image.shape
-        temp = h - 33 + 1
-        if temp % 21 == 0:
-            h = temp + padding2
-        else:
-            h = (h - 33 + 1) // 21 * 21 + 21 + padding2
-        temp = w - 33 + 1
-        if temp % 21 == 0:
-            w = temp + padding2
-        else:
-            w = (w - 33 + 1) // 21 * 21 + 21 + padding2
-        image1 = image[padding2:h, padding2:w]
-    return image1
 
 def modcrop(image, scale=3):
     if len(image.shape) == 3:
@@ -45,16 +16,6 @@ def modcrop(image, scale=3):
         w = w - np.mod(w, scale)
         image = image[0:h, 0:w]
     return image
-
-
-def merge(images, size):
-    h, w = images.shape[1], images.shape[2]
-    img = np.zeros((h * size[0], w * size[1], 1))
-    for idx, image in enumerate(images):
-        i = idx % size[1]
-        j = idx // size[1]
-        img[j * h:j * h + h, i * w:i * w + w, :] = image
-    return img
 
 
 # 归一化和还原
@@ -76,33 +37,13 @@ def preprocess_for_superresolution(img):
     img = im2double(im)
     label_ = modcrop(img, scale=1)
     color_base = modcrop(im, scale=1)
+    color_base = color_base[:, :, 1:3]
     label_ = label_[:, :, 0]
     label_ = scipy.ndimage.interpolation.zoom(label_, (1. / 1), prefilter=False)
     label_ = scipy.ndimage.interpolation.zoom(label_, (1 / 1.), prefilter=False)
-    color_small = modcrop_small(color_base[:, :, 1:3])
-    return label_, color_small
-
-
-def parpare_for_superresolution(sess, img):
-    sub_input_sequence = []
-    label_, color = preprocess_for_superresolution(img)
-    if len(label_.shape) == 3:
-        h, w, _ = label_.shape
-    else:
-        h, w = label_.shape
-    nx = 0
-    ny = 0
-    for x in range(0, h - 33 + 1, 21):
-        nx += 1
-        ny = 0
-        for y in range(0, w - 33 + 1, 21):
-            ny += 1
-            sub_input = label_[x:x + 33, y:y + 33]
-            sub_input = sub_input.reshape([33, 33, 1])
-            sub_input_sequence.append(sub_input)
-    color = np.array(color)
-    data = np.asarray(sub_input_sequence)
-    return data, color, nx, ny
+    data = np.array(label_).reshape([1, img.shape[0], img.shape[1], 1])
+    color = np.array(color_base).reshape([1, img.shape[0], img.shape[1], 2])
+    return data, color
 
 
 def preprocess_for__upscaling(img, scale):
@@ -111,36 +52,10 @@ def preprocess_for__upscaling(img, scale):
     im = scipy.misc.imresize(im, [size[0] * scale, size[1] * scale], interp='bicubic')
     im = cv.cvtColor(im, cv.COLOR_BGR2YCR_CB)
     img = im2double(im)
-    label_ = modcrop(img, scale=scale)
-    color_base = modcrop(im, scale=scale)
+    label_ = modcrop(img, scale=1)
+    color_base = modcrop(im, scale=1)
+    color_base = color_base[:, :, 1:3]
     label_ = label_[:, :, 0]
-    color_small = modcrop_small(color_base[:, :, 1:3])
-    return label_, color_small
-
-
-def parpare_for_upscaling(sess, img, scale):
-    sub_input_sequence = []
-    sub_label_sequence = []
-    label_, color = preprocess_for__upscaling(img, scale)
-    if len(label_.shape) == 3:
-        h, w, _ = label_.shape
-    else:
-        h, w = label_.shape
-    nx = 0
-    ny = 0
-    for x in range(0, h - 33 + 1, 21):
-        nx += 1
-        ny = 0
-        for y in range(0, w - 33 + 1, 21):
-            ny += 1
-            sub_input = label_[x:x + 33, y:y + 33]
-            sub_label = label_[x + 6:x + 6 + 21,
-                        y + 6:y + 6 + 21]
-            sub_input = sub_input.reshape([33, 33, 1])
-            sub_label = sub_label.reshape([21, 21, 1])
-            sub_input_sequence.append(sub_input)
-            sub_label_sequence.append(sub_label)
-    color = np.array(color)
-    data = np.asarray(sub_input_sequence)
-    label = np.asarray(sub_label_sequence)
-    return data, label, color, nx, ny
+    data = np.array(label_).reshape([1, im.shape[0], im.shape[1], 1])
+    color = np.array(color_base).reshape([1, im.shape[0], im.shape[1], 2])
+    return data, color
